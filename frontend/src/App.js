f4ecefabc9b91e26ee00f2e80771e3606d400208
Saurabh <saurabh.tomar@gmail.com>
2024-09-22 22:20:30 -0600

    init front end

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className={"fixed flex w-full bg-[#282c34] text-white p-1 gap-2"}>
+        <h1 className={"text-4xl flex-grow "}>Logistics</h1>
+      </header>
+      <section className={"flex-1 text-white bg-[#1d2025] mt-12"}></section>
+    </div>
+  );
+}
+
+export default App;
